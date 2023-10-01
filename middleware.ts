d7d83048f9bf056ec43ebb664d48f90920d60c55
KhushnudEshtemirov <khushnudeshtemirov@gmail.com>
2023-10-01 16:26:24 +0500

    Updatd

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,3 @@
 export { default } from "next-auth/middleware";
 
-import createMiddleware from "next-intl/middleware";
-
-// export default createMiddleware({
-//   locales: ["uz", "en", "ru"],
-
-//   defaultLocale: "uz",
-// });
-
 export const config = { matcher: ["/profile", "/admin"] };
