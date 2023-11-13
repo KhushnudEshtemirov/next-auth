6ac81928b0e809930488566223862ae56bf6c35f
khushnudeshtemirov <khushnudeshtemirov@gmail.com>
2023-11-13 11:05:17 +0500

    Updated

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,6 +5,8 @@ import { useTranslations } from "next-intl";
 export default function Home() {
   const t = useTranslations("header");
 
+  console.log(process.env.NEXT_PUBLIC_BASE_URL);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>{t("home")}</h1>
