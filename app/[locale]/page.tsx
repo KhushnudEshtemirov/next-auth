c0ae176f4c0da91c97bc08beecc3d2778b3a9a3e
Khushnud Eshtemirov <khushnudeshtemirov@gmail.com>
2024-02-20 12:57:26 +0500

    Added google-recaptcha

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,15 +1,39 @@
 "use client";
 
 import { useTranslations } from "next-intl";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
+import GoogleCaptchaWrapper from "../google-captcha-wrapper";
 
 export default function Home() {
-  const t = useTranslations("header");
+  return (
+    <GoogleCaptchaWrapper>
+      <HomeInside />
+    </GoogleCaptchaWrapper>
+  );
+}
 
-  console.log(process.env.NEXT_PUBLIC_BASE_URL);
+function HomeInside() {
+  const t = useTranslations("header");
+  const { executeRecaptcha } = useGoogleReCaptcha();
+
+  const handleSubmitForm = function (e: any) {
+    e.preventDefault();
+    if (!executeRecaptcha) {
+      console.log("Execute recaptcha not available yet");
+      console.log(
+        "Execute recaptcha not available yet likely meaning key not recaptcha key not set"
+      );
+      return;
+    }
+    executeRecaptcha("enquiryFormSubmit").then((gReCaptchaToken) => {
+      console.log(gReCaptchaToken);
+    });
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>{t("home")}</h1>
+      <button onClick={handleSubmitForm}>Click</button>
     </main>
   );
 }
